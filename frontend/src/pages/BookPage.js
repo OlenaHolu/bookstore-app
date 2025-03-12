84816ad5069bafcff031b015aeb6762b5a35ea8c
Olena <elena.ggrif@gmail.com>
2025-03-12 16:28:43 +0100

    fix design for medium screen

--- frontend/src/pages/BookPage.js
+++ frontend/src/pages/BookPage.js
@@ -1,10 +1,7 @@
-import { useParams } from "react-router-dom"
 import BookDetails from "../components/BookDetails";
 import Layout from "../components/Layout";
 
 const BookPage = () => {
-    const { isbn } = useParams();
-
     return (
         <Layout>
             <BookDetails />
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -10,11 +10,11 @@ const Home = () => {
         <Layout>
             <main className="flex-grow container mx-auto p-4">
                 <div className="flex flex-wrap -mx-4">
-                    <div className="md:w-1/4 bg-white p-4 shadow-md rounded-lg">
+                    <div className="lg:w-1/4 bg-white p-4 shadow-md rounded-lg">
                         <h2 className="text-2xl font-bold mb-4">Añadir Libro</h2>
                         <AddBookForm onBookAdded={() => setRefresh(!refresh)} />
                     </div>
-                    <div className="md:w-3/4 bg-white p-4 shadow-md rounded-lg">
+                    <div className="lg:w-3/4 bg-white p-4 shadow-md rounded-lg">
                         <h2 className="text-2xl font-bold mb-4">Lista de Libros</h2>
                         <BookList key={refresh} />
                     </div>
